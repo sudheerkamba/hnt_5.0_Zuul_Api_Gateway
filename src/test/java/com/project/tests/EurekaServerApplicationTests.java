740132f044fd5cb1f81c3bafd44131a77e65c81e
sudheerkamba <95210080+sudheerkamba@users.noreply.github.com>
2022-01-04 12:09:23 +0530

    Direct commit

---
+++ src/test/java/com/project/tests/EurekaServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.tests;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EurekaServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
